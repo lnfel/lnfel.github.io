aa21b5efa4a061f671be97a468e2fd973a5ed44a
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-03-20 09:45:32 +0800

    [Feature] classList action

---
+++ src/lib/action/index.js
@@ -0,0 +1,16 @@
+/**
+ * @see {@link https://github.com/sveltejs/svelte/issues/3105#issuecomment-1868458487}
+ * @see {@link https://svelte.dev/docs/special-elements#svelte-body | svelte:body}
+ * @type {import('svelte/action').Action<HTMLElement, String | String[]>}
+ */
+export const classList = (node, classes) => {
+    const tokens = Array.isArray(classes) ? classes : classes.split(" ")
+    node.classList.add(...tokens)
+    console.log(tokens)
+
+    return {
+        destroy() {
+            node.classList.remove(...tokens)
+        }
+    }
+}

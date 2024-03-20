0d93a88353477c05cac5ab6a906cce06d4ef3ae2
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-03-20 09:48:07 +0800

    [Feature] Header component now accepts class prop

--- src/routes/(horizontal)/+layout.svelte
+++ src/routes/(horizontal)/+layout.svelte
@@ -139,6 +139,8 @@
 </svelte:head>
 
 <svelte:body use:classList={"bg-indigo-50 dark:bg-slate-900 scroll-smooth"}/>
+
+<Header class="bg-slate-900/50">
     <button on:click={toggleMenu} type="button" class="md:hidden text-tulip-tree-400 outline-none hover:bg-tulip-tree-400 focus:bg-tulip-tree-400 hover:text-slate-900 focus:text-slate-900 transition-colors p-2">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
         <span class="sr-only">Toggle menu</span>

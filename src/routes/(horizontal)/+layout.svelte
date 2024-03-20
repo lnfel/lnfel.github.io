3710fb1159e14c0f721d7c0d75638b0f31c22be1
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-03-20 09:46:26 +0800

    [Change] Set body class using classList action

--- src/routes/(horizontal)/+layout.svelte
+++ src/routes/(horizontal)/+layout.svelte
@@ -8,6 +8,7 @@
 
     import { classToggle } from "$lib/utils"
     import { normalizeWheel } from "$lib/utils/dom"
+    import { classList } from "$lib/action"
 
     import Header from "$lib/components/Header.svelte"
     import LamyDebugbar from "lamy-debugbar"
@@ -137,7 +138,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.bundle.min.js" integrity="sha512-YcsIPGdhPK4P/uRW6/sruonlYj+Q7UHWeKfTAkBW+g83NKM+jMJFJ4iAPfSnVp7BKD4dKMHmVSvICUbE/V1sSw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </svelte:head>
 
-<Header>
+<svelte:body use:classList={"bg-indigo-50 dark:bg-slate-900 scroll-smooth"}/>
     <button on:click={toggleMenu} type="button" class="md:hidden text-tulip-tree-400 outline-none hover:bg-tulip-tree-400 focus:bg-tulip-tree-400 hover:text-slate-900 focus:text-slate-900 transition-colors p-2">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
         <span class="sr-only">Toggle menu</span>

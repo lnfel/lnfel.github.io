b99569469f5d5e16a82e698cf9e4a03311eb03b0
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-03-27 09:26:07 +0800

    [Change] Adjust contact list colors on light theme

--- src/routes/(horizontal)/+page.svelte
+++ src/routes/(horizontal)/+page.svelte
@@ -292,7 +292,7 @@
                     <Contact name="github" text="github.com/lnfel" href="https://github.com/lnfel">
                         <svg slot="icon" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 md:w-10 md:h-10">
                             <title>GitHub</title>
-                            <circle class="hidden group-hover:block" cx="12.5" cy="12.5" r="11" fill="white"/>
+                            <circle class="hidden dark:group-hover:block fill-black dark:fill-white" cx="12.5" cy="12.5" r="11"/>
                             <path fill="currentColor" class="group-hover:fill-[#181717]" d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/>
                         </svg>
                     </Contact>
@@ -455,37 +455,78 @@
         transition: all 200ms ease-in-out;
     }
 
+    /* Contacts */
     :root {
         --github-fill: #181717;
-        --gmail-fill: #EA4335;
-        --discord-fill: #5865F2;
+        --gmail-fill: #ea4435af;
+        --discord-fill: #5865f2c1;
+        --dark-github-fill: #181717;
+        --dark-gmail-fill: #EA4335;
+        --dark-discord-fill: #5865F2;
     }
-
-    /* Contacts */
     :global(.contact.github:has(.contact-link:hover) > div svg circle),
     :global(.contact.github:has(.contact-link:focus) > div svg circle),
     :global(.contact.github:has(button:hover) > div svg circle),
     :global(.contact.github:has(button:focus) > div svg circle) {
         display: block;
     }
+
     :global(.contact.github:has(.contact-link:hover) > div svg path),
     :global(.contact.github:has(.contact-link:focus) > div svg path),
     :global(.contact.github:has(button:hover) > div svg path),
     :global(.contact.github:has(button:focus) > div svg path) {
-        fill: var(--github-fill);
+        fill: white;
     }
+
+    :global(.contact.gmail > div svg path),
+    :global(.contact.gmail > div svg path),
+    :global(.contact.gmail > div svg path),
+    :global(.contact.gmail > div svg path) {
+        fill: var(--gmail-fill);
+    }
+
+    :global(.contact.discord > div svg path),
+    :global(.contact.discord > div svg path),
+    :global(.contact.discord > div svg path),
+    :global(.contact.discord > div svg path) {
+        fill: var(--discord-fill);
+    }
+
+    @media (prefers-color-scheme: dark) {
+        :global(.contact.github:has(.contact-link:hover) > div svg path),
+        :global(.contact.github:has(.contact-link:focus) > div svg path),
+        :global(.contact.github:has(button:hover) > div svg path),
+        :global(.contact.github:has(button:focus) > div svg path) {
+            fill: var(--dark-github-fill);
+        }
+        :global(.contact.gmail > div svg path),
+        :global(.contact.gmail > div svg path),
+        :global(.contact.gmail > div svg path),
+        :global(.contact.gmail > div svg path) {
+            fill: currentColor;
+        }
+
+        :global(.contact.discord > div svg path),
+        :global(.contact.discord > div svg path),
+        :global(.contact.discord > div svg path),
+        :global(.contact.discord > div svg path) {
+            fill: currentColor;
+        }
+    }
+
     :global(.contact.gmail:has(.contact-link:hover) > div svg path),
     :global(.contact.gmail:has(.contact-link:focus) > div svg path),
     :global(.contact.gmail:has(button:hover) > div svg path),
     :global(.contact.gmail:has(button:focus) > div svg path) {
-        fill: var(--gmail-fill);
+        fill: var(--dark-gmail-fill);
     }
     :global(.contact.discord:has(.contact-link:hover) > div svg path),
     :global(.contact.discord:has(.contact-link:focus) > div svg path),
     :global(.contact.discord:has(button:hover) > div svg path),
     :global(.contact.discord:has(button:focus) > div svg path) {
-        fill: var(--discord-fill);
+        fill: var(--dark-discord-fill);
     }
+
     :global(.contact:has(.contact-link:focus) button) {
         opacity: 1;
         pointer-events: auto;
